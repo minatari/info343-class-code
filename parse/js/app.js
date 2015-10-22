fd9eee4c0d8281bcc8c32249bef010bfd8e8024a
Mina <minatari@uw.edu>
2015-10-22 16:53:37 -0700

    after class 10/22

--- parse/js/app.js
+++ parse/js/app.js
@@ -13,9 +13,13 @@ $(function() {
     //new query that will return all tasks ordered by createAt
     var tasksQuery = new Parse.Query(Task);
     tasksQuery.ascending('createdAt'); // automatically comes with new parse object
-
+    tasksQuery.notEqualTo('done', true);
+//    tasksQuery.destory().then(success, destory)
     //reference to the task list element
     var tasksList = $('#tasks-list');
+
+    //reference to our rating element
+    var ratingElem = $('#rating');
 
     //reference to the error message alert
     var errorMessage = $('#error-message'); //question to ask: can you html a jquery?
@@ -57,11 +61,26 @@ $(function() {
     function renderTasks() {
         tasksList.empty();
         tasks.forEach(function(task) {
-            $(document.createElement('li'))
+            var li = $(document.createElement('li'))
                 .text(task.get('title'))
-                .appendTo(tasksList);
+                .addClass(task.get('done') ? 'completed-task' : '') //if task done, add completed-task, else add nothing
+                .appendTo(tasksList)
+                .click(function() {
+                    task.set('done', !task.get('done')); //toggle between true and false
+                    task.save().then(renderTasks, displayError);
+                });
+            $(document.createElement('span'))
+                .raty({readOnly: true,
+                    score: (task.get('rating') || 5), //actual rating, or what you want to default to
+                    hints: ['meh', 'ehh', 'aight', 'cool', 'heart eyes emoji']})
+                .appendTo(li);
         });
     }
+    function showMessage(message) {
+        message = message || 'Hello';
+        alert(message);
+    }
+    //showMessage('World');
 
     //when the user submits the new task form...
     $('#new-task-form').submit(function(evt) { //pass in event object
@@ -69,10 +88,14 @@ $(function() {
 
         var titleInput = $(this).find('[name="title"]');
         var title = titleInput.val();
+
         var task = new Task();
         task.set('title', title);
+        task.set('rating', ratingElem.raty('score')); //help
+
         task.save().then(fetchTasks, displayError).then(function() {
             titleInput.val('');
+            ratingElem.raty('set', {});
         });
 
         return false;
@@ -80,6 +103,9 @@ $(function() {
 
     //go and fetch tasks from the server
     fetchTasks();
+
+    //enable rating user interface element
+    ratingElem.raty(); //.raty({readOnly: true}) for just showing stuff
 
     //window.setInterval(fetchTasks, 10000) //refers to overall browser window
 });
