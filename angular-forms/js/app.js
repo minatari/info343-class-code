53af56014e9d19ded04137b5988d7eb97243ecb1
Mina <minatari@uw.edu>
2015-11-05 16:59:04 -0800

    after class 11/5

--- angular-forms/js/app.js
+++ angular-forms/js/app.js
@@ -5,13 +5,14 @@
 angular.module('ContactsApp', ['ui.router', 'angular-uuid', 'LocalStorageModule'])
     .constant('storageKey', 'contacts-list') //name and a value you want available to other controllers/factories created
     .factory('contacts', function(uuid, localStorageService, storageKey) {//fancy way of fetching data and making them available to multiple controllers
-        return [{
-            id: 'default-delete-me',
-            fname: 'Peggy',
-            lname: 'Carter',
-            phone: '360-444-9169',
-            dob: '4/9/1921'
-        }];
+        return localStorageService.get(storageKey) || [];
+        //return [{
+        //    id: 'default-delete-me',
+        //    fname: 'Peggy',
+        //    lname: 'Carter',
+        //    phone: '360-444-9169',
+        //    dob: '4/9/1921'
+        //}];
     })
     .config(function($stateProvider, $urlRouterProvider) { //make sure spelling is exact //declares our various ui-routes and partials, when to show them, etc
         $stateProvider
@@ -34,6 +35,20 @@ angular.module('ContactsApp', ['ui.router', 'angular-uuid', 'LocalStorageModule'
 
         $urlRouterProvider.otherwise('/contacts'); //just reset that the address ends in this
     })
+    // weird ass syntax time
+    // register a directive for custom validation of dates in the past
+    .directive('inThePast', function() {  //camelcase! like constants and factories, give name and function
+        return {
+            require: 'ngModel',
+            link: function(scope, elem, attrs, controller) {
+                controller.$validators.inThePast = function(modelValue) { //modelvalue = what user has typed in
+                    var today = new Date(); //how we declare a date of right meow
+                    return (new Date(modelValue) <= today);
+                }
+            }
+        };
+    })
+
     .controller('ContactsController', function($scope, contacts) {
         $scope.contacts = contacts;
     })
@@ -43,7 +58,8 @@ angular.module('ContactsApp', ['ui.router', 'angular-uuid', 'LocalStorageModule'
             return contact.id === $stateParams.id; //same as /contacts/:id, id is id property of object
         });
     })
-    .controller('EditContactController', function($scope, $stateParams, $state, contacts) {
+    .controller('EditContactController', function($scope, $stateParams, $state,
+                                                  uuid, localStorageService, storageKey, contacts) {
         var existingContact = contacts.find(function(contact) {
             return contact.id === $stateParams.id;
         });
@@ -51,7 +67,16 @@ angular.module('ContactsApp', ['ui.router', 'angular-uuid', 'LocalStorageModule'
         $scope.contact = angular.copy(existingContact);
 
         $scope.save = function() {
-            angular.copy($scope.contact, existingContact); //propogate those properties    (left: source, right: destination)
+            if ($scope.contact.id) {
+                angular.copy($scope.contact, existingContact); //propogate those properties    (left: source, right: destination)
+            } else {
+                $scope.contact.id = uuid.v4(); //i want a version 4 universal id!
+                contacts.push($scope.contact);
+            }
+            localStorageService.set(storageKey, contacts);
+
+
+            //use $state to go back to the list view
             $state.go('list');
         }
     });
--- firebase/js/app.js
+++ firebase/js/app.js
@@ -1,6 +1,26 @@
 
 angular.module('ChatApp', ['firebase'])
-    .constant('firebaseUrl', 'https://info343chat.firebaseio.com/messages')
-    .controller('ChatController', function($scope) {
-        //TODO: implement our chat controller
+    .constant('firebaseUrl', 'https://info343chat.firebaseio.com/messages') //
+    .controller('ChatController', function($scope, $firebaseArray, firebaseUrl) {
+        //create a reference to the firebase
+        var ref = new Firebase(firebaseUrl);
+        ref.limitToLast(1000);
+        $scope.messages = $firebaseArray(ref); //creates syncronized array. anytime local array changes, server side will change, sync back to local
+
+        //initialize form fields
+        $scope.name = null;
+        $scope.body = null;
+
+        $scope.sendMessage = function() {
+            //adds a new object to the array and synchronizes with the server
+            $scope.messages.$add({ //firebase array, has a special method of add
+                name: $scope.name,
+                body: $scope.body,
+                createdAt: Firebase.ServerValue.TIMESTAMP //server will replace with current date/time on server
+            });
+
+            $scope.body = null;
+        }; //sendMessage() ends
+
+
     });
